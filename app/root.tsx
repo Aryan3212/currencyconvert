4ca634d39a436fa607a731e2f24e1ee284b07301
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2025-01-07 11:45:32 +0600

    feat: google ads

--- app/root.tsx
+++ app/root.tsx
@@ -22,6 +22,8 @@ export const links: LinksFunction = () => [
   },
 ];
 
+
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -30,6 +32,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2360156394549010"
+     crossOrigin="anonymous" />
       </head>
       <body>
         {children}
