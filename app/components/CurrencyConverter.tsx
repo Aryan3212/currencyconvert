ec52d06b52eb134d4a5a91aadfb633a3fb94923c
Aryan Rahman <rahman.aryan07@gmail.com>
2025-01-26 17:37:04 +0600

    feat: fix

--- app/components/CurrencyConverter.tsx
+++ app/components/CurrencyConverter.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { Plus, XIcon } from "lucide-react";
-import { LoaderCircle } from 'lucide-react';
+import { LoaderCircle } from "lucide-react";
 import { Button } from "~/components/ui/button";
 import {
   Command,
@@ -24,7 +24,7 @@ type ValueType = Currency[keyof Currency];
 type CurrencyState = ValueType & {
   displayValue: string;
   error: boolean;
-}
+};
 export default function CurrencyConverter({
   currencyMap,
 }: {
@@ -36,86 +36,119 @@ export default function CurrencyConverter({
   let anchor = 0;
 
   const [currencies, setCurrencies] = useState<CurrencyState[]>([
-    { ...usd,
-      displayValue: usd.value.toFixed(2).toString(),
-      error: false,
-    },
-    { ...eur,
-      displayValue: eur.value.toFixed(2).toString(),
-        error: false,
-     },
-    { ...thb,
-      displayValue: thb.value.toFixed(2).toString(),
-        error: false,
-     },
+    { ...usd, displayValue: usd.value.toFixed(2).toString(), error: false },
+    { ...eur, displayValue: eur.value.toFixed(2).toString(), error: false },
+    { ...thb, displayValue: thb.value.toFixed(2).toString(), error: false },
   ]);
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(true);
-  useEffect(() => {
+  // Function to process localStorage and extract currency data
+  const processLocalStorageCurrencies = () => {
     try {
-      const savedCurrencyList = window.localStorage.getItem('savedCurrencyList')
-      if (!savedCurrencyList) return;
+      const savedCurrencyList =
+        window.localStorage.getItem("savedCurrencyList");
+      if (!savedCurrencyList) {
+        return null;
+      }
 
-      const savedListObj = JSON.parse(savedCurrencyList) as CurrencyState[]
-      if (!Array.isArray(savedListObj) || savedListObj.length === 0) return;
+      const savedListObj = JSON.parse(savedCurrencyList) as CurrencyState[];
+      if (!Array.isArray(savedListObj) || savedListObj.length === 0) {
+        return null;
+      }
 
-      const anchorCurrencyLocal = window.localStorage.getItem('anchor');
-      let anchor = anchorCurrencyLocal ? JSON.parse(anchorCurrencyLocal) : savedListObj[0]?.code;
-      
-      if (!anchor || !savedListObj[anchor] || !currencyMap[savedListObj[anchor].code]) return;
+      const anchorCurrencyLocal = window.localStorage.getItem("anchor");
+      let anchor = anchorCurrencyLocal
+        ? JSON.parse(anchorCurrencyLocal)
+        : savedListObj[0]?.code;
+
+      if (
+        !anchor ||
+        !savedListObj[anchor] ||
+        !currencyMap[savedListObj[anchor].code]
+      ) {
+        return null;
+      }
 
       const anchorCurrency = savedListObj[anchor];
-      const changePercentage = (anchorCurrency.value - currencyMap[savedListObj[anchor].code].value) / currencyMap[savedListObj[anchor].code].value;
+      const changePercentage =
+        (anchorCurrency.value - currencyMap[savedListObj[anchor].code].value) /
+        currencyMap[savedListObj[anchor].code].value;
 
-      Object.entries(currencyMap).forEach((currencyObjPair) => {
-        const code = currencyObjPair[1].code;
-        const changedValue = currencyMap[code].value + currencyMap[code].value * changePercentage;
-        currencyMap[code].value = changedValue;
+      // Create a copy of currencyMap to avoid direct mutation
+      const updatedCurrencyMap = { ...currencyMap };
+
+      Object.entries(updatedCurrencyMap).forEach(([code, currency]) => {
+        const changedValue = currency.value + currency.value * changePercentage;
+        updatedCurrencyMap[code].value = changedValue;
       });
 
-      const updatedList = savedListObj.map(currency => ({
+      const updatedList = savedListObj.map((currency) => ({
         ...currency,
-        ...currencyMap[currency.code],
-        displayValue: currencyMap[currency.code].value.toFixed(2).toString(),
+        ...updatedCurrencyMap[currency.code],
+        displayValue: updatedCurrencyMap[currency.code].value
+          .toFixed(2)
+          .toString(),
         error: false,
       }));
 
-      setCurrencies(updatedList);
-      setLoading(false);
+      return updatedList;
     } catch (error) {
-      console.error('Error loading saved currencies:', error);
+      console.error("Error loading saved currencies:", error);
+      return null;
     }
-  }, [])
+  };
+
+  // In your component
+  useEffect(() => {
+    // Process localStorage and get the result
+    const processedCurrencies = processLocalStorageCurrencies();
+
+    // Update state if processed currencies exist
+    if (processedCurrencies) {
+      setCurrencies(processedCurrencies);
+    }
+
+    // Always set loading to false after processing
+    setLoading(false);
+  }, []);
   const handleValueChange = (index: number, newValue: string) => {
     const validationResult = numberValidator.safeParse(newValue);
     const changedCurrency = currencies[index]; // initial
     if (validationResult.success) {
-      const changePercentage = (validationResult.data - changedCurrency.value) / changedCurrency.value; // -0.1%
+      const changePercentage =
+        (validationResult.data - changedCurrency.value) / changedCurrency.value; // -0.1%
       Object.entries(currencyMap).forEach((currencyObjPair) => {
-        const changedValue = currencyMap[currencyObjPair[1].code].value + currencyMap[currencyObjPair[1].code].value * changePercentage
-        currencyMap[currencyObjPair[1].code].value = changedValue
-      })
+        const changedValue =
+          currencyMap[currencyObjPair[1].code].value +
+          currencyMap[currencyObjPair[1].code].value * changePercentage;
+        currencyMap[currencyObjPair[1].code].value = changedValue;
+      });
       currencies.forEach((currency, currencyIndex, array) => {
         array[currencyIndex] = {
           ...currency,
           ...currencyMap[currency.code],
           error: false,
-        }
+        };
         if (currencyIndex === index) {
-          array[currencyIndex].displayValue = newValue
+          array[currencyIndex].displayValue = newValue;
         } else {
-          array[currencyIndex].displayValue = currencyMap[currency.code].value.toFixed(2).toString()
+          array[currencyIndex].displayValue = currencyMap[currency.code].value
+            .toFixed(2)
+            .toString();
         }
-      });  
+      });
     } else {
-      currencies[index].error = true
-      currencies[index].displayValue = newValue
+      currencies[index].error = true;
+      currencies[index].displayValue = newValue;
     }
     const updatedCurrencyList = [...currencies];
-  setCurrencies(updatedCurrencyList);
-  window.localStorage.setItem('savedCurrencyList', JSON.stringify(updatedCurrencyList));
-  anchor = index;
-  window.localStorage.setItem('anchor', JSON.stringify(index));
+    setCurrencies(updatedCurrencyList);
+    window.localStorage.setItem(
+      "savedCurrencyList",
+      JSON.stringify(updatedCurrencyList)
+    );
+    anchor = index;
+    window.localStorage.setItem("anchor", JSON.stringify(index));
   };
 
   const addCurrency = (currency: Currency[string]) => {
@@ -127,57 +160,68 @@ export default function CurrencyConverter({
         flag: currency.flag,
         code: currency.code,
         displayValue: currency.value.toFixed(2).toString(),
-        error: false
+        error: false,
       },
-    ]
+    ];
     setCurrencies(addedCurrencyList);
-    window.localStorage.setItem('savedCurrencyList', JSON.stringify(addedCurrencyList))
+    window.localStorage.setItem(
+      "savedCurrencyList",
+      JSON.stringify(addedCurrencyList)
+    );
     setOpen(false);
   };
 
   const removeCurrency = (index: number) => {
-    const filteredCurrencies = currencies.filter((_, i) => i !== index)
+    const filteredCurrencies = currencies.filter((_, i) => i !== index);
     setCurrencies(filteredCurrencies);
-    window.localStorage.setItem('savedCurrencyList', JSON.stringify(filteredCurrencies))
-    window.localStorage.setItem('anchor', JSON.stringify(anchor > currencies.length - 2 ? anchor : 0))
+    window.localStorage.setItem(
+      "savedCurrencyList",
+      JSON.stringify(filteredCurrencies)
+    );
+    window.localStorage.setItem(
+      "anchor",
+      JSON.stringify(anchor > currencies.length - 2 ? anchor : 0)
+    );
   };
 
-  return (
-    loading ? <div className="flex flex-col justify-center items-center h-[360px]">
-    <LoaderCircle className="animate-spin h-8 w-8" />
-    <h1 className="text-center text-sm text-slate-600">Loading saved state..</h1>
+  return loading ? (
+    <div className="flex flex-col justify-center items-center h-[360px]">
+      <LoaderCircle className="animate-spin h-8 w-8" />
+      <h1 className="text-center text-sm text-slate-600">
+        Loading saved state..
+      </h1>
     </div>
-    :
+  ) : (
     <div className="space-y-4">
       {currencies.map((currency, index) => (
-         <div key={index} className="relative">
-         <div className="flex items-center space-x-2 mb-1">
-           <span className="text-2xl" aria-hidden="true">
-             {currency.flag}
-           </span>
-           <span className="font-bold" aria-label={currency.name}>
-             {currency.code}
-           </span>
-           <span className="text-sm text-muted-foreground">
-             {currency.name}
-           </span>
-         </div>
-         <input
-           value={currency.displayValue}
-           onChange={(e) => handleValueChange(index, e.target.value)}
-           className="w-full text-3xl font-medium bg-transparent border-b border-input focus:outline-none focus:border-primary transition-colors"
-           aria-label={`Amount in ${currency.name}`}
-         />
-         <Button
-           variant="ghost"
-           size="icon"
-           onClick={() => removeCurrency(index)}
-           className="absolute right-0 top-8 text-muted-foreground hover:text-foreground hover:bg-red-100 hover:text-red-500"
-           aria-label={`Remove ${currency.name}`}
-         >
-           <XIcon className="h-6 w-6" aria-label="Remove Currency" />
-         </Button>
-         {currency.error && (
+        <div key={index} className="relative">
+          <div className="flex items-center space-x-2 mb-1">
+            <span className="text-2xl" aria-hidden="true">
+              {currency.flag}
+            </span>
+            <span className="font-bold" aria-label={currency.name}>
+              {currency.code}
+            </span>
+            <span className="text-sm text-muted-foreground">
+              {currency.name}
+            </span>
+          </div>
+          <input
+            value={currency.displayValue}
+            onChange={(e) => handleValueChange(index, e.target.value)}
+            className="w-full text-3xl font-medium bg-transparent border-b border-input focus:outline-none focus:border-primary transition-colors"
+            aria-label={`Amount in ${currency.name}`}
+          />
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => removeCurrency(index)}
+            className="absolute right-0 top-8 text-muted-foreground hover:text-foreground hover:bg-red-100 hover:text-red-500"
+            aria-label={`Remove ${currency.name}`}
+          >
+            <XIcon className="h-6 w-6" aria-label="Remove Currency" />
+          </Button>
+          {currency.error && (
             <Alert variant="destructive" className="mt-2">
               <AlertCircle className="h-4 w-4" />
               <AlertTitle>
@@ -185,19 +229,19 @@ export default function CurrencyConverter({
               </AlertTitle>
             </Alert>
           )}
-         </div>
+        </div>
       ))}
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-full justify-start text-white hover:text-white bg-primary hover:bg-primary/90 text-lg py-6"
-        >
-          <Plus className="mr-2 h-5 w-5" />
-          Add Currency
-        </Button>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-full justify-start text-white hover:text-white bg-primary hover:bg-primary/90 text-lg py-6"
+          >
+            <Plus className="mr-2 h-5 w-5" />
+            Add Currency
+          </Button>
         </PopoverTrigger>
         <PopoverContent className="w-full p-0 text-xl">
           <Command className="text-xl min-w-64">
