0f083d9ef0fddab80d7a8fe2a588bb549ba25a8c
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2025-09-29 15:31:18 +0700

    turn off google ads for now

--- app/components/GoogleAds.tsx
+++ app/components/GoogleAds.tsx
@@ -1,6 +1,7 @@
 export function GoogleAds() {
-  return (
-     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2360156394549010"
-         crossOrigin="anonymous"></script>
+  return (<>
+     { true || <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2360156394549010"
+         crossOrigin="anonymous"></script> }
+         </>
   );
 }
